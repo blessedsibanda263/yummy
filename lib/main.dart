174fa24c7a3fe13a329e06f61ac0db36e6c8aeea
blessedsibanda263 <blessedsibanda263@gmail.com>
2025-03-06 11:14:50 +0200

    Yummy App

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const Yummy());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class Yummy extends StatelessWidget {
+  // TODO: Setup default theme
+
+  //  2
+  const Yummy({super.key});
+
+  // TODO: Add changeTheme above here
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    const appTitle = 'Yummy';
+
+    // TODO: Setup default theme
+
+    // 3
+    return MaterialApp(
+      title: appTitle,
+      // debugshowCheckedModeBanner: false,
+
+      // TODO: Add theme
+
+      // TODO: Apply Home widget
+
+      // 4
       home: Scaffold(
+        appBar: AppBar.new(
+          elevation: 4.0,
+          title: const Text(appTitle, style: TextStyle(fontSize: 24.0)),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('You Hungry?', style: TextStyle(fontSize: 30.0)),
         ),
       ),
     );
